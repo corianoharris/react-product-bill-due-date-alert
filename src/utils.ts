ecb4771f331c637264364e5672628c8a602ba2c5
Coriano Harris <corianoharris@gmail.com>
2023-06-08 14:21:45 -0700

    Added util fns

---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import user from "./MockData/userData";
+
+const dueDate: Date = new Date(`${user.dueDate}`);
+const currentDate: Date = new Date();
+
+// convert future or today date into ms
+export const msBetweenDueDate: number = Math.abs(
+  dueDate.getTime() - currentDate.getTime()
+);
+
+// convert past date into ms
+export const msAfterDueDate: number = Math.abs(
+  currentDate.getTime() - dueDate.getTime()
+);
+
+// convert ms to days for future and today                         hrs  mins sec   ms
+export const daysBetweenDueDate: number = Math.round(
+  msBetweenDueDate / (24 * 60 * 60 * 1000)
+);
+
+// convert ms to days for past                                     hrs  mins sec   ms
+export const daysAfterDueDate: number = Math.round(
+  msAfterDueDate / (24 * 60 * 60 * 1000)
+);
