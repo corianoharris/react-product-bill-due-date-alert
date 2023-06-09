33d25e20b31545f713b400dc9728925736a4e863
Coriano Harris <corianoharris@gmail.com>
2023-06-08 18:38:13 -0700

    Added margin and box shadow to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,13 +7,17 @@ interface ButtonProps {
 
 const StyledButton = styled.button`
   width: auto;
+  margin: 20px 0;
   padding: 20px 10px;
   border: 2px soild black;
   border-radius: 5px;
   background-color: white;
   color: black;
-  letter-spacing: 6px;
+  letter-spacing: 2px;
   font-weight: bold;
+  font-size: 18px;
+  box-shadow: 0 2px 4px -2px rgba(0, 0, 0.16),
+    0 5px 8px -4px rgba(0, 0, 0, 0.16);
 
   &:hover {
     background-color: black;
